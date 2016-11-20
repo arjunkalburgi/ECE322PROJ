e89ee4840d22cd93f86752446a1f9c4b473307b5
Arjun Kalburgi <askalburgi@gmail.com>
2016-11-20 16:58:22 -0700

    stuff

--- tests/database_tests.py
+++ tests/database_tests.py
@@ -4,76 +4,76 @@ from time import strftime
 import pytest
 
 def test_getCurrentTime():
-	assert getCurrentTime() == null
+	assert db.getCurrentTime() == null
 	
-def test_encrypt(s):
-	assert encrypt() == null
+def test_encrypt():
+	assert db.encrypt("ABC") == "CDE"
 
 def test_getUser(username, password):
-	assert getUser() == null
+	assert db.getUser() == null
 
-def test_createUser(role, name, login, password):
-	assert createUser() == null
+def test_createUser():
+	assert db.createUser("D", "name", login, password) == null
 
 def test_getChartsForPatient(patient):
-	assert getChartsForPatient() == null
+	assert db.getChartsForPatient() == null
 
 def test_symptomsForPatientAndChart(hcno, chart_id):
-	assert symptomsForPatientAndChart() == null
+	assert db.symptomsForPatientAndChart() == null
 
 def test_diagnosesForPatientAndChart(hcno, chart_id):
-	assert diagnosesForPatientAndChart() == null
+	assert db.diagnosesForPatientAndChart() == null
 
 def test_medicationsForPatientAndChart(hcno, chart_id):
-	assert medicationsForPatientAndChart() == null
+	assert db.medicationsForPatientAndChart() == null
 
 def test_addSymptomToChart(hcno, chart_id, staff_id, symptom):
-	assert addSymptomToChart() == null
+	assert db.addSymptomToChart() == null
 
 def test_addDiagnosisToChart(hcno, chart_id, staff_id, diagnoses):
-	assert addDiagnosisToChart() == null
+	assert db.addDiagnosisToChart() == null
 
 def test_getPatientWithHcno(hcno):
-	assert getPatientWithHcno() == null
+	assert db.getPatientWithHcno() == null
 
 def test_isMedicationAmountValid(drug_name, amount, age_group):
-	assert isMedicationAmountValid() == null
+	assert db.isMedicationAmountValid() == null
 
 def test_getValidMedicationAmount(drug_name, age_group):
-	assert getValidMedicationAmount() == null
+	assert db.getValidMedicationAmount() == null
 
 def test_isPatientAllergicToDrug(hcno, drug_name):
-	assert isPatientAllergicToDrug() == null
+	assert db.isPatientAllergicToDrug() == null
 
 def test_inferredAllergy(hcno, drug_name):
-	assert inferredAllergy() == null
+	assert db.inferredAllergy() == null
 
 def test_addMedicationToChart(hcno, chart_id, staff_id, start_med, end_med, drug_name, amount):
-	assert addMedicationToChart() == null
+	assert db.addMedicationToChart() == null
 
 def test_createPatient(hcno, name, age_group, address, phone, emg_phone):
-	assert createPatient() == null
+	assert db.createPatient() == null
 
 def test_isChartOpenForPatient(hcno):
-	assert isChartOpenForPatient() == null
+	assert db.isChartOpenForPatient() == null
 
 def test_createNewChartForPatient(hcno):
-	assert createNewChartForPatient() == null
+	assert db.createNewChartForPatient() == null
 
 def test_closeChartWithId(chart_id):
-	assert closeChartWithId() == null
+	assert db.closeChartWithId() == null
 
 def test_drugAmountForEachDoctor(start, end):
-	assert drugAmountForEachDoctor() == null
+	assert db.drugAmountForEachDoctor() == null
 
 def test_drugAmountForEachCategory(start, end):
-	assert drugAmountForEachCategory() == null
+	assert db.drugAmountForEachCategory() == null
 
 def test_totalAmountForEachCategory(start, end):
-	assert totalAmountForEachCategory() == null
+	assert db.totalAmountForEachCategory() == null
 
 def test_listMedicationsForDiagnosis(diagnoses):
-	assert listMedicationsForDiagnosis() == null
+	assert db.listMedicationsForDiagnosis() == null
 
 def test_listDiagnosesMadeBeforePrescribingDrug(drug_name):
-	assert listDiagnosesMadeBeforePrescribingDrug() == null
+	assert db.listDiagnosesMadeBeforePrescribingDrug() == null
