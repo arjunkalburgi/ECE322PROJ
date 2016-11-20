65e6565f53a42c0d3df3b61abbdf9c5c26a70bf3
Arjun Kalburgi <askalburgi@gmail.com>
2016-11-20 16:38:02 -0700

    prep database test file

--- tests/database_tests.py
+++ tests/database_tests.py
@@ -3,8 +3,80 @@ from software.app_files import database as db
 from time import strftime
 import pytest
 
-def __init__(): 
-	db.connectDB()
+def test_dict_factory():
+	
 
-def test_getCurrentTime(): 
-	assert db.getCurrentTime() == strftime("%Y-%m-%d %H:%M:%S")
+def test_getCurrentTime():
+
+	
+def test_encrypt():
+	
+
+def test_getUser():
+	
+
+def test_createUser():
+	
+
+def test_getChartsForPatient():
+	
+
+def test_symptomsForPatientAndChart():
+	
+
+def test_diagnosesForPatientAndChart():
+	
+
+def test_medicationsForPatientAndChart():
+	
+
+def test_addSymptomToChart():
+	
+
+def test_addDiagnosisToChart():
+	
+
+def test_getPatientWithHcno():
+	
+
+def test_isMedicationAmountValid():
+	
+
+def test_getValidMedicationAmount():
+	
+
+def test_isPatientAllergicToDrug():
+	
+
+def test_inferredAllergy():
+	
+
+def test_addMedicationToChart():
+	
+
+def test_createPatient():
+	
+
+def test_isChartOpenForPatient():
+	
+
+def test_createNewChartForPatient():
+	
+
+def test_closeChartWithId():
+	
+
+def test_drugAmountForEachDoctor():
+	
+
+def test_drugAmountForEachCategory():
+	
+
+def test_totalAmountForEachCategory():
+	
+
+def test_listMedicationsForDiagnosis():
+	
+
+def test_listDiagnosesMadeBeforePrescribingDrug():
+	
