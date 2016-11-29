7ffc2c5492ad3c60e5c0c65acbf8fed8bc88f401
Arjun Kalburgi <askalburgi@gmail.com>
2016-11-28 20:04:16 -0700

    beginning of branch tests

---
+++ tests/branch_tests.py
@@ -0,0 +1,19 @@
+# def db.createUser(role, name, login, password):
+# def db.isChartOpenForPatient(hcno):
+# def db.createNewChartForPatient(hcno):
+
+# def classes.Doctor.getCharts(self, patient):
+# def classes.Admin.listMedicationsForDiagnosis(self, diagnosis):
+# def classes.Admin.listDiagnosesMadeBeforePrescribingDrug(self, drug_name):
+
+# def admin.listMedicationsForDiagnosisFlow(adm):
+# def admin.listDiagnosisesForDrugFlow(adm):
+
+# def nurse.getPatientFlow(nur):
+# def nurse.selectChart(nur, patient):
+
+# def doctor.getChartsFlow(doc):
+# def doctor.selectChart(doc, patient):
+# def doctor.addMedicationFlow(doc, patient, chart):
+
+# def login.start():
